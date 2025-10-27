33f977e9368ec7df030dbce47732825e7b6d2533
mrunu1503 <mrunu1503@gmail.com>
2025-10-27 13:24:50 +0530

    u

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,15 +1,22 @@
 export const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
-  const cookieExpireDays = process.env.EXPIRE_COOKIE || 7;
+  const cookieExpireDays = Number(process.env.EXPIRE_COOKIE) || 7;
+
+  const isProduction = process.env.NODE_ENV === "production";
 
   const options = {
     expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", 
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    overwrite: true, 
+    secure: isProduction, 
+    sameSite: isProduction ? "none" : "lax", 
+    path: "/", 
   };
+
+
+  if (isProduction) {
+    options.domain = ".onrender.com";
+  }
 
   res
     .status(statusCode)
